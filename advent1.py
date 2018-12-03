15b577b14fc1e6b9a01f424974b67901ad4be2be
Alina <34385319+alinazhiltsova@users.noreply.github.com>
2018-12-03 13:35:38 +0000

    Add files via upload

--- advent1.py
+++ advent1.py
@@ -1,3 +1,13 @@
 numbers = open('adventday1.txt')
 advent = numbers.read().split('\n')
 print(sum([int(i) for i in advent]))
+
+seen = set()
+f = 0
+while True:
+	for i in advent:
+		f+= int(i)
+		if f in seen:
+			print(f)
+			quit()
+		seen.add(f)
